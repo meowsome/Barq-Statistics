5184272f4f7ad5bf071730e1a2fd206c035672a0
meowsome <tycarr01@gmail.com>
2026-04-17 21:41:59 -0500

    Add title, description, spacing between graphs

--- server/barq-scraping-react-app/app/page.tsx
+++ server/barq-scraping-react-app/app/page.tsx
@@ -14,11 +14,14 @@ export default function App() {
         <Map />
       </div>
       <div className="w-1/3 p-4 overflow-auto h-screen" >
-        <h1 className="text-2xl font-medium">
-          Hello world!
+        <h1 className="text-2xl font-bold mb-2">
+          Barq Scraping Statistics
         </h1>
         
-        <div className="flex flex-wrap">
+        <div className="flex flex-wrap space-y-2">
+          <p className="text-body text-gray-600">
+            Barq is an app for furries to connect with one another based on location. For this project, profile information was collected for a subset of all Barq users. This information was used to perform analytics and statistics. No individual user was singled out in this project.
+          </p>
           <TotalCount />
           <GenderChart />
           <OrientationChart />
