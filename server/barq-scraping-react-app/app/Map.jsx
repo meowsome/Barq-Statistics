9aa058e7f7f2ab3a77f18352a0f50ca23ad95867
meowsome <tycarr01@gmail.com>
2026-04-23 00:33:10 -0500

    Make dropdown for map layers look better

--- server/barq-scraping-react-app/app/Map.jsx
+++ server/barq-scraping-react-app/app/Map.jsx
@@ -2,12 +2,33 @@ import React, { useEffect, useState, useRef } from "react";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import 'leaflet.heat'
+import countries_geojson from './data/countries.json'
+
+const chartColors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
+// Style for countries on the main map
+function style(feature) {
+    return {
+        stroke: false,
+        fillOpacity: 0.5
+    };
+}
 
 export default function Map() {
     const [points, setPoints] = useState("");
     const isInitialized = useRef(false);
     const [visibleLayer, setVisibleLayer] = useState();
     const [map, setMap] = useState();
+    const [menuOpen, setMenuOpen] = useState(false);
 
     function removeActiveLayer() {
         if (visibleLayer) visibleLayer.remove();
@@ -36,21 +57,17 @@ export default function Map() {
             for (let i = 0; i < uniqueSonas.length; i++) {
                 sonasColors[uniqueSonas[i]] = chartColors[i];
             }
-            
-            console.log(uniqueSonas)
-            console.log(sonasColors)
 
             const fursonaLayer = L.geoJson(countries_geojson, {
                 onEachFeature: function(feature, layer) {
                     var countryName = feature.properties.name;
-                    const countryDetails = data.find(country => country._id == countryName);
+                    const countryDetails = data.find(country => country._id == countryName || country._id == "United States" && countryName == "United States of America"); 
                     if (countryDetails != null) {
                         const popularFursona = countryDetails.popularFursona;
-                        console.log(sonasColors[popularFursona]);
                         layer.setStyle({
                             fillColor: sonasColors[popularFursona]
                         })
-                        layer.bindPopup("<b>" + popularFursona + "</b>");
+                        layer.bindTooltip("<b>" + popularFursona + "</b>", {permanent: true});
                     }
                 },
                 style: style
@@ -83,9 +100,24 @@ export default function Map() {
         <div>
             <div className="relative z-0" id="map" style={{ height: "100vh", width: "100%" }}></div>
             
-            <div className="absolute top-3 right-3 z-1">
-                <button className="bg-blue-500 text-white" onClick={() => addHeatmapLayer()}>Heatmap Layer</button>&nbsp;
-                <button className="bg-blue-500 text-white" onClick={() => addFursonaLayer()}>Fursona Layer</button>
+            <div className="absolute top-3 right-3 z-1 text-right">
+                <button data-dropdown-toggle="dropdown" className="inline-flex items-center justify-center text-black shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 cursor-pointer bg-white" type="button" onClick={() => setMenuOpen(!menuOpen)}>
+                    Map Layers
+                    <svg className="w-4 h-4 ms-1.5 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 9-7 7-7-7"/></svg>
+                </button>
+
+                {menuOpen && (
+                    <div id="dropdown" className="z-10 bg-neutral-primary-medium rounded-base shadow-lg w-50 bg-white">
+                        <ul className="p-2 text-sm text-body font-medium" aria-labelledby="dropdownDefaultButton">
+                            <li>
+                                <a className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded cursor-pointer" onClick={() => addHeatmapLayer()}>Furry Locations Heatmap</a>
+                            </li>
+                            <li>
+                                <a className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded cursor-pointer" onClick={() => addFursonaLayer()}>Top Fursona Per Country</a>
+                            </li>
+                        </ul>
+                    </div>
+                )}
             </div>
         </div>
     );
