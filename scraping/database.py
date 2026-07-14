5aab1073c6458aa92af7f3ab00a90a7fb5c8d356
meowsome <tycarr01@gmail.com>
2026-07-14 00:03:04 -0500

    Add basic auth for mongo database

--- scraping/database.py
+++ scraping/database.py
@@ -3,7 +3,12 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-pymongo_client = pymongo.MongoClient(os.getenv("MONGODB_URL"))
+pymongo_client = pymongo.MongoClient(
+    host=os.getenv("MONGODB_URL"),
+    username=os.getenv("MONGODB_USERNAME"),
+    password=os.getenv("MONGODB_PASSWORD"),
+    authSource=os.getenv("MONGODB_DATABASE")
+)
 pymongo_db = pymongo_client[os.getenv("MONGODB_DATABASE")]
 pymongo_col = pymongo_db[os.getenv("MONGODB_COLLECTION")]
 
