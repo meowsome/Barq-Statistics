5cacf6d70e75239b27ef8e111b8c5565f4ec6945
meowsome <tycarr01@gmail.com>
2026-07-14 01:38:19 -0500

    Begin adding mobile layout

--- server/barq-scraping-react-app/app/page.tsx
+++ server/barq-scraping-react-app/app/page.tsx
@@ -9,19 +9,21 @@ const Map = dynamic(() => import("./Map"), {ssr: false});
 
 export default function App() {
   return (
-    <div className="flex">
-      <div className="w-2/3 relative">
+    <div className="flex flex-col sm:flex-row">
+      <div className="h-1/2 sm:h-full w-full sm:w-2/3 relative">
         <Map />
       </div>
-      <div className="w-1/3 p-4 overflow-auto h-screen" >
-        <h1 className="text-2xl font-bold mb-2">
+      <div className="w-full sm:w-1/3 p-4 overflow-auto h-auto md:h-screen" >
+        <h1 className="text-3xl font-bold mb-2">
           Barq Scraping Statistics
         </h1>
         
-        <div className="flex flex-wrap space-y-2">
-          <p className="text-body text-gray-600">
+          <p className="text-body text-gray-600 mb-2">
             Barq is an app for furries to connect with one another based on location. For this project, profile information was collected for a subset of all Barq users. This information was used to perform analytics and statistics. No individual user was singled out in this project.
           </p>
+        
+        <div className="flex flex-wrap flex-col space-y-2">
+          
           <TotalCount />
           <GenderChart />
           <OrientationChart />
